a7544cadaab08da1f891bc770eb98be8f637b1f1
mohammad282001 <32101001006@std.bau.edu.jo>
2024-06-10 15:15:54 +0300

    clearing

--- Donuts Factory/app.js
+++ Donuts Factory/app.js
@@ -25,47 +25,51 @@ if (confirm("Do you want to order?\n (Donuts/ Coffee/ Ice Cream/ Bakery )")) {
     alert("Your order is getting prepared");
 }
 
-console.log("Name: " + name + " order: " + orderType);
 
-let orderArr = [name, gender, orderType]
-for (let i = 0; i < orderArr.length; i++) {
-        console.log(orderArr[i]);
+
+// console.log("Name: " + name + " order: " + orderType);
+
+// let orderArr = [name, gender, orderType]
+// for (let i = 0; i < orderArr.length; i++) {
+//         console.log(orderArr[i]);
     
-}
-let arrNum = [0, 1, 2, 3, 4, 5]
+// }
+// let arrNum = [0, 1, 2, 3, 4, 5]
 
-// Q7: Use a loop to display the numbers 0 through 5, each in a separate alert window.
-for (let i = 0; i<=5; i++) {
-    alert(arrNum[i]);
-}
+// // Q7: Use a loop to display the numbers 0 through 5, each in a separate alert window.
+// for (let i = 0; i<=5; i++) {
+//     alert(arrNum[i]);
+// }
 
-// Q8: Use a loop to display the numbers 0 through 5 in a single alert window
-alert(arrNum);
+// // Q8: Use a loop to display the numbers 0 through 5 in a single alert window
+// alert(arrNum);
 
-// Q9: Use a loop to display the numbers in the range 0…20 that are multiples of 3. 
-let multiThree = []
-for (let i = 1; i <= 20; i++)
-    if (i%3==0)
-        multiThree.push(i);
-alert(multiThree);
+// // Q9: Use a loop to display the numbers in the range 0…20 that are multiples of 3. 
+// let multiThree = []
+// for (let i = 1; i <= 20; i++)
+//     if (i%3==0)
+//         multiThree.push(i);
+// alert(multiThree);
 
-//Q10: Prompt the user for a number in the range 0…100.  
-//If the number is out of range, display an error message and prompt again until a valid number is entered.
-//Assume the user enters a number each time
-let userNum = +prompt("Enter a number between 0-100")
-while (true) {
-    if (userNum >= 0 && userNum <= 100)
-        break;
-    userNum = +prompt("Error, Please enter a number between 0-100")
-}
+// //Q10: Prompt the user for a number in the range 0…100.  
+// //If the number is out of range, display an error message and prompt again until a valid number is entered.
+// //Assume the user enters a number each time
+// let userNum = +prompt("Enter a number between 0-100")
+// while (true) {
+//     if (userNum >= 0 && userNum <= 100)
+//         break;
+//     userNum = +prompt("Error, Please enter a number between 0-100")
+// }
 
-//Q11: Prompt for an integer, then display the sum of the integers from 0 through the number entered
-let n = +prompt("Please enter a number to display the sum from 0 to that number");
-let sum=0;
-for (let i = 0; i <=n; i++) {
-    sum += i;
-}
-alert(sum);
+// //Q11: Prompt for an integer, then display the sum of the integers from 0 through the number entered
+// let n = +prompt("Please enter a number to display the sum from 0 to that number");
+// let sum=0;
+// for (let i = 0; i <=n; i++) {
+//     sum += i;
+// }
+// alert(sum);
+
+
 
 
 // EX4 DOM task
